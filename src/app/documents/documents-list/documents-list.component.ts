1e002408df08e45337b2a609009bc760a919cb1d
kuresti <55415865+kuresti@users.noreply.github.com>
2025-05-14 16:23:28 -0600

    Implement Documents feature

--- src/app/documents/documents-list/documents-list.component.ts
+++ src/app/documents/documents-list/documents-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-documents-list',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './documents-list.component.css'
 })
 export class DocumentsListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+ documents: Document[] = [
+  new Document(1,'CIT 280 ', 'Object Oriented Programming', 'https://content.byui.edu/CIT280', null ),
+  new Document(2,'CIT 366', 'Full Web Stack Development', 'https//content.byui.edu/CIT366', null),
+  new Document(3,'CIT 425', 'Data Warehousing', 'https//content.byui.edu/CIT 425', null),
+  new Document(4,'CIT 480', 'Enterprise Development', 'https//content.byui.edu/CIT480', null),
+  new Document(5, 'CIT 495', 'Senior Practicum', 'https//content.byui.edu/CIT495', null),  
+ ]
+
+ onSelectedDocument(document: Document){
+  this.selectedDocumentEvent.emit(document);
+ }
 }
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Document } from './document.model';
+
 @Component({
   selector: 'cms-documents',
   standalone: false,
@@ -7,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './documents.component.css'
 })
 export class DocumentsComponent {
-
+ selectedDocument: Document;
 }
